20b602ca2a7740aa3bbe4eb71d5c287ebc4c3ed8
Thiago Freitas <freitas.taf@gmail.com>
2024-12-11 21:55:01 -0300

    Incluindo Subtração

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,11 @@
 #Projeto de calculadora
+#Incluindo subtração
 
 a = input('informe um número: ')
 b = input('informe um número: ')
 
 soma= a + b
 print(f'Soma: {soma}')
+
+subtracao = a - b
+print(f'Subtração: {subtracao}')
